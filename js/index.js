d1a41585edcdb4b37d397ad078099370bec4d7ef
Erichy-dev <83208823+Erichy-dev@users.noreply.github.com>
2021-08-04 16:22:53 +0300

    initializing the new game

--- js/index.js
+++ js/index.js
@@ -4,11 +4,22 @@ class SceneA extends Phaser.Scene {
   }
   preload(){
     this.load.setPath('assets/');
-    this.load.image('background', 'background.png');
-    this.load.image('bomb', 'mine.png');
+    this.load.image('background');
+    this.load.atlas('squirrel')
+    this.load.image('mine');
   }
   create(){
     this.background = this.add.tileSprite(512, 384, 1024, 768, 'background');
+
+    
+    this.anims.create({
+      key: 'squirrel-run',
+      frames: this.anims.generateFrameNames('squirrel'),
+      repeat: -1,
+      frameRate: 10,
+    });
+    
+    this.squirrel = this.add.sprite(120, 720, 'squirrel').setScale(0.7).setTint('black').play('squirrel-run');
   }
   update (){
     this.background.tilePositionX += 2;
