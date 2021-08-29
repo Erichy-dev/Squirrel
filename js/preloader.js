926352691f75c993ac6fa35146dda5ec64db3cb6
Erichy-dev <83208823+Erichy-dev@users.noreply.github.com>
2021-08-29 09:56:32 +0300

    github pages bug

--- js/preloader.js
+++ js/preloader.js
@@ -6,10 +6,8 @@ export class Preloader extends Phaser.Scene{
   }
   preload(){
     //Start scene
-    this.load.atlas('squirrel', '../assets/squirrel.png', '../assets/squirrel.json');
-    this.load.setPath('../assets/AudioFile/');
+    this.load.atlas('squirrel', 'squirrel.png', 'squirrel.json');
     this.load.audio('hip-latin', 'hip-latin-jazz.mp3');
-    this.load.setPath('../assets/StartScene/');
     this.load.image('light', 'light.png');
     this.load.atlas('candle');
     this.load.image('backgroundd', 'backgroundd.png');
@@ -23,9 +21,8 @@ export class Preloader extends Phaser.Scene{
     this.load.image('start');
 
     // squirrel scene
-    this.load.setPath('assets/');
-    this.load.audio('red-moon', 'AudioFile/red-moon.mp3');
-    this.load.atlas('cheetah', 'enemy/cheetah.png', 'enemy/cheetah.json');
+    this.load.audio('red-moon', 'red-moon.mp3');
+    this.load.atlas('cheetah', 'cheetah.png', 'cheetah.json');
     this.load.image('background');
     this.load.atlas('squirrel');
     this.load.plugin('rexpathfollowerplugin', 'rexpathfollowerplugin.min.js', true);
